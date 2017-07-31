e98a250b98be3f1944061144450691e22d62ba03
Zack Hartmann <zackhartmann@gmail.com>
2017-07-31 15:35:50 -0400

    AJAX Queries Users - Creates Array

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $( function() {
 	$( "#tabs" ).tabs();
 } );
+
+$(document).ready(function() {
+	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	var usersArray = [];
+	$.each(users, function (index, user) {
+		$.ajax( {
+			type: 'GET',
+			dataType: 'json',
+			// json requires {url + '?callback=?'}, but jsonp works standing alone.
+			url: '//wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?',
+			success: function(responseData) {
+				usersArray.push(responseData);
+			}
+		});
+	})
+	console.log(usersArray);
+});
