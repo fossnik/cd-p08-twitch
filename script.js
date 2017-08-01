78fd24c721128d6e23b4d8925632351e534b846d
Zack Hartmann <zackhartmann@gmail.com>
2017-08-01 01:46:45 -0400

    Two Ajaxes

--- script.js
+++ script.js
@@ -1,63 +1,45 @@
-$( function() {
-	$( "#tabs" ).tabs();
-} );
+function eachUser(users) {
+	$.each(users, function(index, user) {
+		doAJAXuser(user);
+		userOnline(user);
+	});
+};
+
+function doAJAXuser(user) {
+	$.ajax({
+		dataType: 'json',
+		url: '//wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?',
+		success: function(responseData) {
+			usersOutput({
+				id: responseData._id,
+				display_name: responseData.display_name,
+				logo: responseData.logo,
+				link: responseData._links.self
+			}); // usersOutput
+		} // success
+	}); // ajax
+};
+
+function userOnline(user) {
+	$.ajax({
+		dataType: 'json',
+		url: '//wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?',
+		success: function(responseData) {
+			if(responseData.stream === null) {
+				console.log(user + ": No Active Streams");
+			} else {
+				console.log(user + ": Steaming!");
+			};
+		} // success
+	}); // ajax
+};
+
+function usersOutput(idNameLogoLink) {
+	console.log(idNameLogoLink.logo);
+	$('#tabs-1').html(idNameLogoLink.id + "\n" + idNameLogoLink.display_name);
+};
 
 $(document).ready(function() {
-	// var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp"];
-	var usersArray = [];
-	$.each(users, function(index, user) {
-		$.ajax( {
-			// async: false,
-			type: 'GET',
-			// json requires {url + '?callback=?'}, but jsonp works standing alone.
-			dataType: 'json',
-			url: '//wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?',
-			// dataType: 'jsonp',
-			// url: '//wind-bow.gomix.me/twitch-api/users/' + user,
-			success: function(responseData) {
-				outsideFunction({
-					id: responseData._id,
-					display_name: responseData.display_name,
-					logo: responseData.logo,
-					link: responseData._links.self
-				}); // usersArray.push
-			} // success
-		}); // ajax
-	}); // each
-	// console.log("usersArray variable:\n", usersArray);
-	// console.log("usersArray.length: ", usersArray.length);
-	function outsideFunction(res) {
-		console.log(res)
-		usersArray.push(res);
-	}
-	// 	console.log()
-	// usersArray.forEach(function(element) {
-	// 	console.log(element);
-	// });
-	// $.each(usersArray, function(index, element) {
-	// 	console.log(usersArray[index]);
-	// });
-
-	// PSUEDOCODE:
-	// foreach (user)
-	// allusers =
-	// IF user is ONLINE:
-	// htmlOnline += user
-	// else
-	// htmlOffline += user
-
-
-	// // append the tabs-1 (aka ONLINE)
-	// $.each(usersArray, function(index, userObj) {
-	// 	htmltabs =
-	// })
-	// // append the tabs-2 (aka OFFLINE)
-	// $.each(usersArray, function(index, userObj) {
-	// 	htmltabs =
-	// })
-	// // append the tabs-3 (aka ALL)
-	// $.each(usersArray, function(index, userObj) {
-	// 	htmltabs =
-	// })
+	$( "#tabs" ).tabs();
+	eachUser(["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]);
 });
