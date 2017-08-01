85a6dde708d5790f5e98f621f71637cdb2e02511
Zack Hartmann <zackhartmann@gmail.com>
2017-07-31 23:05:15 -0400

    confusingly outsideFunction

--- script.js
+++ script.js
@@ -3,18 +3,61 @@ $( function() {
 } );
 
 $(document).ready(function() {
-	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	// var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp"];
 	var usersArray = [];
-	$.each(users, function (index, user) {
+	$.each(users, function(index, user) {
 		$.ajax( {
+			// async: false,
 			type: 'GET',
-			dataType: 'json',
 			// json requires {url + '?callback=?'}, but jsonp works standing alone.
+			dataType: 'json',
 			url: '//wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?',
+			// dataType: 'jsonp',
+			// url: '//wind-bow.gomix.me/twitch-api/users/' + user,
 			success: function(responseData) {
-				usersArray.push(responseData);
-			}
-		});
-	})
-	console.log(usersArray);
+				outsideFunction({
+					id: responseData._id,
+					display_name: responseData.display_name,
+					logo: responseData.logo,
+					link: responseData._links.self
+				}); // usersArray.push
+			} // success
+		}); // ajax
+	}); // each
+	// console.log("usersArray variable:\n", usersArray);
+	// console.log("usersArray.length: ", usersArray.length);
+	function outsideFunction(res) {
+		console.log(res)
+		usersArray.push(res);
+	}
+	// 	console.log()
+	// usersArray.forEach(function(element) {
+	// 	console.log(element);
+	// });
+	// $.each(usersArray, function(index, element) {
+	// 	console.log(usersArray[index]);
+	// });
+
+	// PSUEDOCODE:
+	// foreach (user)
+	// allusers =
+	// IF user is ONLINE:
+	// htmlOnline += user
+	// else
+	// htmlOffline += user
+
+
+	// // append the tabs-1 (aka ONLINE)
+	// $.each(usersArray, function(index, userObj) {
+	// 	htmltabs =
+	// })
+	// // append the tabs-2 (aka OFFLINE)
+	// $.each(usersArray, function(index, userObj) {
+	// 	htmltabs =
+	// })
+	// // append the tabs-3 (aka ALL)
+	// $.each(usersArray, function(index, userObj) {
+	// 	htmltabs =
+	// })
 });
